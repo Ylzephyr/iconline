8090d02fa128e0f06014b7e9c9cb829b807eed00
Ylzephyr <zephyr339@outlook.com>
2022-04-15 11:36:06 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+import IndexPage from './pages/Index';
+
+ReactDOM.render(
+    <IndexPage/>,
+    document.getElementById("app"),
+);
---
+++ src/pages/Index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Box from '@mui/material/Box'
+
+
+export default function IndexPage() {
+    return (
+        <Box
+            sx={{
+                width: '100%',
+                height: '100%',
+                backgroundColor: 'primary.dark',
+            }}
+        />
+    )
+}
